c111f31efd460542c6f92aba62b365b5c8e033d0
Claudia <claudiabarcelovaldes40@gmail.com>
2023-12-25 22:46:47 +0100

    make briadcast attk function virtual to allow override it

--- script/LevelAttacks/Broadcasted.sol
+++ script/LevelAttacks/Broadcasted.sol
@@ -2,14 +2,14 @@
 
 pragma solidity ^0.8.19;
 
-import {console, Script} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 abstract contract Broadcasted is Script {
     function attack(address payable falloutCtr) public payable virtual;
 
     function broadcastedAttack(
         address payable levelInstanceCtr_
-    ) external payable {
+    ) external payable virtual {
         // start broadcst to sign the transactions as the msg.sender(our address)
         vm.startBroadcast();
         attack(levelInstanceCtr_);
