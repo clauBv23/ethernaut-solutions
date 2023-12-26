bfa666dff168e6be8e472b4acd546ffb7ca95968
Claudia <claudiabarcelovaldes40@gmail.com>
2023-12-26 16:36:38 +0100

    refactor

--- script/RunLvlAttack.sol
+++ script/RunLvlAttack.sol
@@ -21,17 +21,18 @@ contract RunLvlAttack is Script {
     uint256 constant c_someEther = 0.00001 ether;
     uint256 constant c_etherValue = 0.001 ether;
     // address in Sepolia
-    address constant EthernautCtr = 0xa3e7317E591D5A0F1c605be1b3aC4D2ae56104d6;
-    address constant lvl1Factory = 0x3c34A342b2aF5e885FcaA3800dB5B205fEfa3ffB;
-    address constant lvl2Factory = 0x676e57FdBbd8e5fE1A7A3f4Bb1296dAC880aa639;
-    address constant lvl3Factory = 0xA62fE5344FE62AdC1F356447B669E9E6D10abaaF;
-    address constant lvl4Factory = 0x2C2307bb8824a0AbBf2CC7D76d8e63374D2f8446;
-    address constant lvl5Factory = 0x478f3476358Eb166Cb7adE4666d04fbdDB56C407;
-    address constant lvl6Factory = 0x73379d8B82Fda494ee59555f333DF7D44483fD58;
-    address constant lvl7Factory = 0xb6c2Ec883DaAac76D8922519E63f875c2ec65575;
-    address constant lvl8Factory = 0xB7257D8Ba61BD1b3Fb7249DCd9330a023a5F3670;
-    address constant lvl9Factory = 0x3049C00639E6dfC269ED1451764a046f7aE500c6;
-    address constant lvl10Factory = 0x2a24869323C0B13Dff24E196Ba072dC790D52479;
+    address constant ETHERNAUT_CTR = 0xa3e7317E591D5A0F1c605be1b3aC4D2ae56104d6;
+    address constant LVL_1_FACTORY = 0x3c34A342b2aF5e885FcaA3800dB5B205fEfa3ffB;
+    address constant LVL_2_FACTORY = 0x676e57FdBbd8e5fE1A7A3f4Bb1296dAC880aa639;
+    address constant LVL_3_FACTORY = 0xA62fE5344FE62AdC1F356447B669E9E6D10abaaF;
+    address constant LVL_4_FACTORY = 0x2C2307bb8824a0AbBf2CC7D76d8e63374D2f8446;
+    address constant LVL_5_FACTORY = 0x478f3476358Eb166Cb7adE4666d04fbdDB56C407;
+    address constant LVL_6_FACTORY = 0x73379d8B82Fda494ee59555f333DF7D44483fD58;
+    address constant LVL_7_FACTORY = 0xb6c2Ec883DaAac76D8922519E63f875c2ec65575;
+    address constant LVL_8_FACTORY = 0xB7257D8Ba61BD1b3Fb7249DCd9330a023a5F3670;
+    address constant LVL_9_FACTORY = 0x3049C00639E6dfC269ED1451764a046f7aE500c6;
+    address constant LVL_10_FACTORY =
+        0x2a24869323C0B13Dff24E196Ba072dC790D52479;
 
     // todo could be easier to use but will imply storing all lvls on storage
     // mapping(uint256 lvlNumber => address lvlFactory) lvlFactories;
@@ -71,7 +72,7 @@ contract RunLvlAttack is Script {
     ) public returns (address payable) {
         // get lvl instance
         vm.recordLogs();
-        IEthernaut(EthernautCtr).createLevelInstance{value: createValue_}(
+        IEthernaut(ETHERNAUT_CTR).createLevelInstance{value: createValue_}(
             lvlFactory_
         );
         Vm.Log[] memory _entries = vm.getRecordedLogs();
@@ -99,7 +100,7 @@ contract RunLvlAttack is Script {
             )
         );
         // submit lvl instance
-        IEthernaut(EthernautCtr).submitLevelInstance(payable(lvlInstance_));
+        IEthernaut(ETHERNAUT_CTR).submitLevelInstance(payable(lvlInstance_));
     }
 
     function attackLevel(
@@ -125,51 +126,51 @@ contract RunLvlAttack is Script {
     {
         if (lvlNumber_ == 1) {
             console.log("01 Fallback level attack");
-            lvlFactory = lvl1Factory;
+            lvlFactory = LVL_1_FACTORY;
             lvlAttack = new FallbackAttk();
             callValue = 2 * c_someEther;
         } else if (lvlNumber_ == 2) {
             console.log("02 Fallout level attack");
-            lvlFactory = lvl2Factory;
+            lvlFactory = LVL_2_FACTORY;
             lvlAttack = new FalloutAttk();
         }
         // else if (lvlNumber_ == 3) {
         // todo lvl3 need calls on different blocks look a workaround
         //     console.log("03 Coin Flip level attack");
-        //     return (lvl3Factory, new CoinFlipAttk(), 0);
+        //     return (LVL_3_FACTORY, new CoinFlipAttk(), 0);
         // }
         else if (lvlNumber_ == 4) {
             console.log("04 Telephone level attack");
-            lvlFactory = lvl4Factory;
+            lvlFactory = LVL_1_FACTORY;
             lvlAttack = new TelephoneAttk();
             needBroadcast = true;
         } else if (lvlNumber_ == 5) {
             console.log("05 Token level attack");
-            lvlFactory = lvl5Factory;
+            lvlFactory = LVL_5_FACTORY;
             lvlAttack = new TokenAttack();
         } else if (lvlNumber_ == 6) {
             console.log("06 Delegation level attack");
-            lvlFactory = lvl6Factory;
+            lvlFactory = LVL_6_FACTORY;
             lvlAttack = new DelegationAttk();
         } else if (lvlNumber_ == 7) {
             console.log("07 Force level attack");
-            lvlFactory = lvl7Factory;
+            lvlFactory = LVL_7_FACTORY;
             lvlAttack = new ForceAttk{value: c_someEther}();
             needBroadcast = true;
         } else if (lvlNumber_ == 8) {
             console.log("08 Vault level attack");
-            lvlFactory = lvl8Factory;
+            lvlFactory = LVL_8_FACTORY;
             lvlAttack = new VaultAttk();
         } else if (lvlNumber_ == 9) {
             console.log("09 King level attack");
-            lvlFactory = lvl9Factory;
+            lvlFactory = LVL_9_FACTORY;
             lvlAttack = new KingAttk();
             callValue = c_etherValue;
             needBroadcast = true;
             createValue = c_etherValue;
         } else if (lvlNumber_ == 10) {
             console.log("10 Reentrancy level attack");
-            lvlFactory = lvl10Factory;
+            lvlFactory = LVL_10_FACTORY;
             lvlAttack = new ReentrancyAttk();
             callValue = c_etherValue;
             needBroadcast = true;
